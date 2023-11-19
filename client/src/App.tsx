e167a111ce6b01f5e0821689df04e30239613de2
1willcobb <cobb.will@gmail.com>
2023-11-18 19:17:02 -0800

    starting back up

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { Home } from './Pages'
-import  WordAnimation  from './Components/test'
 import {Footer, Header} from './Components'
 import './App.css'
 
@@ -9,7 +8,6 @@ function App() {
   return (
     <Router>
       <Header/>
-      <WordAnimation/>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
