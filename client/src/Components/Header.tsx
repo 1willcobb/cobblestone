1b373367fc319127e719b917cfb3b2a0c2e0615e
1willcobb <cobb.will@gmail.com>
2023-11-18 19:25:30 -0800

    links for header

--- client/src/Components/Header.tsx
+++ client/src/Components/Header.tsx
@@ -1,10 +1,18 @@
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>
+    <div className="flex bg-gray-400 w-full">
       <h1>Header</h1>
+      <Link to="/">Home</Link>
+      <Link to="/about">Photography</Link>
+      <Link to="/about">Videography</Link>
+      <Link to="/investment">Investment</Link>
+      <Link to="/journal">Journal</Link>
+      <Link to="/about">About Us</Link>
+      <Link to="/contact">Contact Us</Link>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- client/src/Pages/Home/index.tsx
+++ client/src/Pages/Home/index.tsx
@@ -1,9 +1,8 @@
-import React from "react";
 
 const Home = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Homepage</h1>
+      <h1 className="text-3xl font-bold">Homepage</h1>
     </div>
   );
 };
