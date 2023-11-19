8d09b82fe740301a7ebc3db00aada653bd85623d
1willcobb <cobb.will@gmail.com>
2023-11-18 21:15:39 -0800

    styling home bar

--- client/src/Components/Header.tsx
+++ client/src/Components/Header.tsx
@@ -2,16 +2,35 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div className="flex bg-gray-400 w-full">
-      <h1>Header</h1>
-      <Link to="/">Home</Link>
-      <Link to="/about">PHOTOGRAPHY</Link>
-      <Link to="/about">Videography</Link>
-      <Link to="/investment">Investment</Link>
-      <Link to="/journal">Journal</Link>
-      <Link to="/about">About Us</Link>
-      <Link to="/contact">Contact Us</Link>
-      <h1 className="font-gopher_black">Header HERE</h1>
+    <div className="flex bg-midTan w-full justify-between p-5">
+      <div className="flex gap-2">
+        <Link to="/" className="text-lightTan hover:text-tan">
+          Home
+        </Link>
+        <Link to="/about" className="text-lightTan hover:text-tan">
+          Photography
+        </Link>
+        <Link to="/about" className="text-lightTan  hover:text-tan">
+          Videography
+        </Link>
+        <Link to="/investment" className="text-lightTan  hover:text-tan">
+          Investment
+        </Link>
+        <Link to="/journal" className="text-lightTan  hover:text-tan">
+          Journal
+        </Link>
+        <Link to="/about" className="text-lightTan  hover:text-tan">
+          About Us
+        </Link>
+        <Link to="/contact" className="text-lightTan  hover:text-tan">
+          Contact Us
+        </Link>
+      </div>
+      <div className="flex">
+        <Link to="/">
+          <h1 className="font-gopher hover:opacity-50">COBBLESTONE</h1>
+        </Link>
+      </div>
     </div>
   );
 };
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,14 +2,21 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      fontFamily: {
-        "gopher": ["gopher_black", "ariel", "sans-serif"],
-        "cryene": ["cryene", "ariel", "sans-serif"],
-        "philosopher": ["philosopher", "ariel", "sans-serif"],
-        "airhead": ["airhead", "ariel", "sans-serif"],
-      },
+    colors: {
+      lightTan: "#FFF6EA",
+      tan: "#FFE6C6",
+      midTan: "#E9B878",
+      darkTan: "#906935",
+      orangePop: "#FF9900",
+      neverBlack: "#2e2e2e",
     },
+    fontFamily: {
+      gopher: ["gopher", "ariel", "sans-serif"],
+      cryene: ["cryene", "ariel", "sans-serif"],
+      philosopher: ["philosopher", "ariel", "sans-serif"],
+      airhead: ["airhead", "ariel", "sans-serif"],
+    },
+    extend: {},
   },
 
   plugins: [],
